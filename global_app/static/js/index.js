f693bd3540a09ea7b876b984fda8249b1e33b20b
Rodrigo <116903783+Rodrigo635@users.noreply.github.com>
2025-11-11 22:31:14 -0300

    Ajuste de comentários

--- global_app/static/js/index.js
+++ global_app/static/js/index.js
@@ -1,3 +1,4 @@
+// Smooth scroll
 document.querySelectorAll('a[href^="#"]').forEach(a => {
     a.addEventListener('click', function (e) {
         const target = document.querySelector(this.getAttribute('href'));
